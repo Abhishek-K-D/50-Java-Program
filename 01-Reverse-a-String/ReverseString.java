fdd715b2661e00d3f5a74b0c1c08549d896cf363
Abhi <abhishekdileep71@gmail.com>
2026-08-18 23:00:24 +0530

    Added Program 01 - Reverse a String

--- 01-Reverse-a-String/ReverseString.java
+++ 01-Reverse-a-String/ReverseString.java
@@ -1,5 +1,13 @@
-package 01-Reverse-a-String;
-
+import java.util.*;
 public class ReverseString {
-    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+        for(int i = str.length() - 1 ; i>=0 ; i--){
+            System.out.print(str.charAt(i));
+        }
+        System.out.println();
+        sc.close();
+    }
 }
